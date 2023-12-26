000ca4e55171c03c38d17aad594a15614440eac8
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-12-26 15:59:46 -0800

    Setup Context and connected app with firebase

---
+++ src/context/Firebase.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from 'react';
+import { initializeApp } from 'firebase/app';
+const FirebaseContext = createContext(null);
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6YcbSs3ixhxZzESxhDMz1tpe9izPQSHo',
+  authDomain: 'bookstore-app-b270a.firebaseapp.com',
+  projectId: 'bookstore-app-b270a',
+  storageBucket: 'bookstore-app-b270a.appspot.com',
+  messagingSenderId: '158202548625',
+  appId: '1:158202548625:web:50270796dc43a5fad68a30',
+  measurementId: 'G-18PMMV0NB7',
+};
+
+export const useFirebase = useContext(FirebaseContext);
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseProvider = (props) => {
+  return <FirebaseContext.Provider>{props.children}</FirebaseContext.Provider>;
+};
