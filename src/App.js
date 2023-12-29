a0d21d9cc44bc4a4b034c3572d8cf2a9b413fdc0
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-12-28 20:12:02 -0800

    Created Navbar and List page alongwith create book listing functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 
+// Components
+import MyNavbar from './components/Navbar';
+
 // Pages
 import RegisterPage from './pages/Register';
 import LoginPage from './pages/Login';
+import ListingPage from './pages/List';
 
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-    </Routes>
+    <div>
+      <MyNavbar />
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/book/list' element={<ListingPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -8,7 +8,8 @@ import {
   signInWithPopup,
   onAuthStateChanged,
 } from 'firebase/auth';
-
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
 const FirebaseContext = createContext(null);
 
 // Your web app's Firebase configuration
@@ -27,6 +28,8 @@ export const useFirebase = () => useContext(FirebaseContext);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -48,6 +51,23 @@ export const FirebaseProvider = (props) => {
 
   const signinWithGoogle = () => signInWithPopup(firebaseAuth, googleProvider);
 
+  console.log(user);
+
+  const handleCreateNewListing = async (name, isbn, price, cover) => {
+    const imageRef = ref(storage, `uploads/images/${Date.now()}-${cover.name}`);
+    const uploadResult = await uploadBytes(imageRef, cover);
+    return await addDoc(collection(firestore, 'books'), {
+      name,
+      isbn,
+      price,
+      imageURL: uploadResult.ref.fullPath,
+      userID: user.uid,
+      userEmail: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+    });
+  };
+
   const isLoggedIn = user ? true : false;
 
   return (
@@ -56,6 +76,7 @@ export const FirebaseProvider = (props) => {
         signupUserWithEmailAndPassword,
         signinUserWithEmailAndPassword,
         signinWithGoogle,
+        handleCreateNewListing,
         isLoggedIn,
       }}
     >
